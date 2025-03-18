91794058972287c25ac75919d208092a5fc29cae
shuhratkarimov <shuhratkarimov.dev@gmail.com>
2025-03-18 21:55:47 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 4001 || 4002;
+  const PORT = process.env.PORT || 4005
   await app.listen(PORT, () => {
     console.log("Server is running on the port: " + PORT);
   });
